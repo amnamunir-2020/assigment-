65ca5c016e92bd794c33aefdc361acbbd7caa2a5
amnamunir-2020 <59399151+amnamunir-2020@users.noreply.github.com>
2020-07-05 21:40:45 +0500

    HelpDesk2-3
    code chapters javascript

---
+++ help.js
@@ -0,0 +1,108 @@
+// JavaScript Document
+
+
+//flag-------for  condition --var flag="false"
+//------------helpdesk2-----------------------------------------------------
+
+//function mat(a,b=8){
+//	alert(a+b)
+//}
+//
+//mat(5)
+//mat(5,4)
+//mat(5)NAN
+
+//First letter Capital   Amna
+//var a="amna"
+//var cap=a.slice(0,1).toUpperCase()+a.slice(1,a.length)
+//document.write(cap)
+
+
+//back one
+//var cal="2+4-7+6"
+//var calculation=cal.slice(0,-1)
+//document.write(calculation)
+
+//calculator back number
+//function back(){
+//	var result=document.getElementById("result")
+//	//console.log(result.value)
+//	result.value=result.value.slice(0,-1)
+//}
+
+
+
+//
+//var userName="@mna."
+//for(i=0;i<userName.length;i++){
+//	if(userName[i].charCodeAt()===64||userName[i].charCodeAt()===33||userName[i].charCodeAt()===46)
+//		alert("UserName is not Valid")
+//}
+
+
+//function getValue(){
+//	alert("test")
+//}
+//
+//function setValue(){
+//	var name=document.getElementById("name")
+//	name.value="faryaal"
+//	console.log(name.value)
+//}
+
+
+//function setValue(){
+//	var paragraph=document.getElementById("para")
+//	paragraph.innerHTML="This is example !!"
+//	
+//}
+
+//var name=document.getElementById("name");
+//name.value
+
+
+//function bigImage(){
+//	var image=document.getElementById("img")
+//	image.className="big"
+//}
+//function smallImage(){
+//	var image=document.getElementById("img")
+//	image.className="small"
+//}
+
+
+//function openDoor(){
+//	var door=document.getElementById("door");
+//	door.src="open.png"
+//}
+//
+//function closeDoor(){
+//	var door=document.getElementById("door");
+//	door.src="close.png"
+//}
+
+//
+//function onBulb(){
+//	var bulbFunction=document.getElementById("bulb");
+//	bulbFunction.src="onbulb.jpg"
+//}
+//
+//function offBulb(){
+//	var bulbFunction=document.getElementById("bulb");
+//	bulbFunction.src="offbulb.jpg"
+//}
+
+
+
+//function addClass(){
+//	var para=document.getElementById("para")
+//	para.className +=" bold purple"    //multiples classes kailia hamae space den ahoga sath mai + equal kiasath
+//}
+
+
+function getElement(){
+	var element=document.getElementsByTagName("p")
+	console.log(element)
+}
+
+getElement()
