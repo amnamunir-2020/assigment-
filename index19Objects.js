1351d08d9e5b8453666b42bbcbc9ef42c32ec89d
amnamunir-2020 <59399151+amnamunir-2020@users.noreply.github.com>
2020-07-24 03:23:58 +0500

    class19.1CodeObject

---
+++ index19Objects.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+//ARRAY KAI UNDER OBJECT GENERATE!!!!!!!!!!!!!!!!!!!!1and OBJECT KAI UNDER BI OBJECT
+// var myQuestions[
+//	{
+//		question:"What is name?",
+//		answer:{
+//			a:"Ali",
+//			b:"Ghous",
+//			c:"Basit"
+//		},
+//		correctAnswer:"c"
+//	},
+//	{
+//		question:"What is age?",
+//		answer:{
+//			a:12,
+//			b:34,
+//			c:24
+//		},
+//		correctAnswer:"c"
+//	},
+//	
+//
+//]
+//CONSTRUCTOR FUNCTION::::::::::::::::::::::::::::::;
+function Course(t1,t2,t3){
+	this.t1=t1;
+	this.t2=t2;
+	this.t3=t3;
+	
+}
+//Object Instance Create!!!!!!!!
+var xyz=new Course("ALI","GHOUS","BASIT")
+console.log(xyz)
+	
+	//Not necessart parameter   Property be any thing
+function Course(t1,t2,t3){
+	this.a=t1;
+	this.b=t2;
+	this.c=t3;
+	
+}
+//Object Instance Create!!!!!!!!
+var ijk=new Course("SIR ALI"," SIR GHOUS"," SIR BASIT")
+console.log(ijk)
+	
