a72e5e6ce9e0c7ddfe2439bdeca7a0f30c6300a6
amnamunir-2020 <59399151+amnamunir-2020@users.noreply.github.com>
2020-08-24 04:57:27 +0500

    codeclass22
    chapters BROWSER CONTOL AND LINUX---COMMAND NOTE IN REGISTER~!!!

---
+++ app22.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+function press(){
+	//location
+	//var locate=window.location.href
+	//console.log(locate)
+	
+	//window.location.href='https://www.google.com/'
+	//bestpractice
+	//window.location.assign('https://www.google.com/')
+	//back not page
+	//window.location.replace('https://www.google.com/')
+	
+	//reload page cache reloading window.location.reload(false or empty) 
+	//window.location.reload()
+	
+	//server reloading
+	//window.location.reload(true)
+	
+	//console.log(window.location.pathname)
+	
+	//window.open()
+	
+	
+//	var content="<h1>Successfully Page Change!!</h1>"
+//	window.open().document.write(content)
+
+	
+	window.open('https://github.com/amnamunir-2020/assigment-')
+	
+	
+}
+
+
+//inconsole 
+//window.location.pathname
+//"/ServiceLogin/signinchooser"
+//window.location.hostname
+//"www.google.com"
+
+//tab open searching any
+//history.back()
+//history.forward()
+//if no forward page
+//history.forward()
+//undefined
+//history.go(1)  //go 1 forward page
+//history.go(-1)   //go 1 backward page
+//history.go(-2)  
+
+//kiswebsite sai user ayaa refer
+//document.referrer
+//"https://www.google.com/
+
+
+
+
+
