770c8df1106dcd1c8ff5befaae3da9fdc0a60d76
amnamunir-2020 <59399151+amnamunir-2020@users.noreply.github.com>
2020-09-06 00:39:14 +0500

    Helpdesk1,2,3,4,5,6,7

--- help.js
+++ help.js
@@ -127,11 +127,7 @@
 //     var cartprice=document.getElementById("cart-price")
 //	 cartimg.src=img.src
 //	 cartname.innerHTML=name.innerHTML
-//	 cartprice.innerHTML=price.innerHTML
-//	
-	 
-	 
-	 
+//	 cartprice.innerHTML=price.innerHTML	 
 //    console.log(img.src)
 //	console.log(name.innerHTML)
 //	console.log(price.innerHTML)
@@ -142,10 +138,160 @@
 //-------------------------Helpdesk5------------------------
 
 
-//MODAL IMAGE..............................................
-function showImage(e){
-	console.log(e)
-	var modalImage=document.getElementById("modalImage")
-	modalImage.src=e.src
-	
-}
+////MODAL IMAGE..............................................
+//function showImage(e){
+//	console.log(e)
+//	var modalImage=document.getElementById("modalImage")
+//	modalImage.src=e.src
+//	
+//}
+
+
+
+//------------helpdesk6-----------------------------------------------------
+//var arr=["faryal","waliya"]
+//console.log(arr[2])
+
+
+//<!--OBJECT-----	-->
+//	var student={
+//		Name:"Amna",
+//		SeatNo:1810007,
+//		Study:"MCS"
+//		
+//	}
+//	console.log(student)
+//console.log(student.Name)
+//console.log(student.SeatNo)
+
+//array kai under object:-''''''''''''''''''''''''''
+
+//var students=[{name:"Ghous"},{name:"Basit"}]
+//console.log(students[1])
+//console.log(students[1].name)
+//multiples-------
+//var students=[{name:"Ghous"},{name:"Basit"}]
+//for(var i=0; i<students.length;i++)
+//	{
+//		console.log(students[i].name)
+//	}
+
+//-------
+//var student={
+//		Name:"Amna",
+//		SeatNo:1810007,
+//	Study:"MCS"
+//	
+//}
+//object !property  addkerna kai tariqa ========
+//student.phone=03438000382
+//console.log(student)
+//--------------------
+//object mai update full property:::::::
+
+//student={
+//Name:"Faryaal",
+//SeatNo:1810002,
+//	Study:"MCS-Evening"
+//}
+//console.log(student)
+
+
+//or variable mai update
+//var Name:"Faryaal"
+//Name="Sadia"
+
+//undefined properties::::::
+//student={
+//Name:"Faryaal",
+//SeatNo:1810002,
+//	Study:"MCS-Evening"
+//}
+//console.log(student.phone)
+//
+
+
+
+//======empty object then add properties
+//var student={
+//}
+//
+//student.name="Walii Shah"
+//console.log(student)
+
+//deleteproperties using delete keyword------------------
+//var student={
+//	name:"sdcwdj",
+//	phone:0848987
+//}
+//delete student.name
+//console.log(student)
+
+//check properties::::::::::::::::::::::::;
+//var student={
+//	name:"sdcwdj",
+//	phone:0848987
+//}
+//////object kai under property majood hai kai nahi???/
+//var  check="name" in  student;
+//var  check="RollNo" in  student;
+////ans---TRUE/FALSE
+//console.log(check)
+//			
+
+
+//OBJECT KAI UNDER ARRAY ::::OR FUNCTION bi::::
+//var student={
+//	name:"marium",
+//phone:0848987,
+//	subjects:["English","Urdu","Math"],
+//	sub1:90,
+//	sub2:80,
+//	getResult:function(){
+//		//this bi istemaal kersaktai hai
+//		//return this.sub1+student.sub2
+//		return student.sub1+student.sub2
+//	}
+//	
+//}
+//console.log(student.getResult())
+
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;OBJECT METHODS:
+
+//objeck kai under function use ho tou wo METHOD kehlaata hai!!!!!jasai yahn getMarks ka function>
+//Object mia prperty name hai function ki name nahi likhtai function kai gai :
+
+//var student={
+//	name:"marium",
+//phone:0848987,
+//	
+//	getMarks:function(){
+//		return 600
+//	}
+//	
+//}
+//console.log(student.getMarks())
+
+//barbar structure n a banaan parai islia constructor same properties or value alg dertai reho///////////////////////////////////CONSTRUCTOR FUNCTION:
+
+//4 property or constructor function ki pehchannn function name Capital sai start
+//this function ko target kereha hai
+
+//function Student(name,className,RollNo,Phone){
+//this.anyName=parameter
+//this.StudentName=name;
+//	this.name=name;
+//	this.className=className;
+//	this.RollNo=RollNo;
+//	this.Phone=Phone;
+//	
+//	
+//}
+//new KEYWORD for constructor
+//call constructor function
+//multiple objects same properties different value
+//var student1=new Student("Ghous","Web And Mobile",9086,46124767)
+//var student2=new Student("Basit","AI",76556,4612499877)
+//console.log(student1)
+//console.log(student2)
+
